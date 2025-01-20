adfee78436ff470825b4ba3affff46fd05d154d2
Anudhin <anudhinthomas@gmail.com>
2025-01-19 19:53:42 -0500

    Lab01 code

---
+++ Week_01/Lab/lab1.py
@@ -0,0 +1,16 @@
+# Name: Anudhin Thomas
+
+
+numbers = [1, 4, 7, 9]
+
+a, b, c, d = 1, 2, 3, 4
+
+e = (a * c) - (b / d)
+
+e = ((a - ((b ** c) // d)) + (a % c))
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter your age: ")) + 22
+print("Now showing the shop items filtered by age:", userAge)
